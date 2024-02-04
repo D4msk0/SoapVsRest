5b5516897d2bfc50e26ec1defc7d69947fae74e4
Merry Damsko <m.d4msk0@gmail.com>
2024-02-04 10:52:17 +0100

    added GreetingService

--- src/main/java/com/damsko/SoapVsRest/service/GreetingService.java
+++ src/main/java/com/damsko/SoapVsRest/service/GreetingService.java
@@ -1,4 +1,11 @@
 package com.damsko.SoapVsRest.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingService {
+
+    public String generateGreeting(String name){
+        return "Hello, " + name + "!";
+    }
 }
