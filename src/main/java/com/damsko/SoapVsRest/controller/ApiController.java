a87a2db32fce5e2639584b7a9fdd3190a9333ceb
Merry Damsko <m.d4msk0@gmail.com>
2024-02-04 12:45:08 +0100

    rest works... soap does not. Not sure how to approach SOAP, perhaps buy the book.

--- src/main/java/com/damsko/SoapVsRest/controller/ApiController.java
+++ src/main/java/com/damsko/SoapVsRest/controller/ApiController.java
@@ -1,21 +1,19 @@
 package com.damsko.SoapVsRest.controller;
 
+
 import com.damsko.SoapVsRest.model.Greeting;
-import com.damsko.SoapVsRest.service.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 
-    @Autowired
-    private GreetingService greetingService;
-
-    @GetMapping("/api/greet/{name}")
-    public Greeting greet(@PathVariable String name){
-        String message = greetingService.generateGreeting(name);
-        return new Greeting(message);
+    @GetMapping("/greeting")
+    public Greeting getRestGreeting(){
+        Greeting greeting = new Greeting();
+        greeting.setMessage("Hello from REST!");
+        return greeting;
     }
 }
--- src/main/java/com/damsko/SoapVsRest/controller/SoapEndpoint.java
+++ src/main/java/com/damsko/SoapVsRest/controller/SoapEndpoint.java
@@ -1,22 +1,20 @@
 package com.damsko.SoapVsRest.controller;
 
 import com.damsko.SoapVsRest.model.Greeting;
-import com.damsko.SoapVsRest.service.SoapService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-public class SoapController {
+@Endpoint
+public class SoapEndpoint {
 
     private static final String NAMESPACE_URI = "http://www.damsko.com/soapvsrest";
 
-    @Autowired
-    private SoapService soapService;
-
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getGreetingRequest")
     @ResponsePayload
-    public Greeting getGreeting() {
-        String message = soapService.generateSoapGreeting("SOAP User");
-        return new Greeting(message);
+    public Greeting getSoapGreeting() {
+        Greeting greeting = new Greeting();
+        greeting.setMessage("Hello from SOAP!");
+        return greeting;
     }
 }
