b8b5f8c1f7873741eb631702a915a9fd750bae61
Merry Damsko <m.d4msk0@gmail.com>
2024-02-04 10:00:29 +0100

    first commit.

---
+++ src/test/java/com/damsko/SoapVsRest/SoapVsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damsko.SoapVsRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapVsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
