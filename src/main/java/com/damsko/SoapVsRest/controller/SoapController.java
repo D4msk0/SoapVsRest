68077f9152568fbfe832af238a78b56f8611e06b
Merry Damsko <m.d4msk0@gmail.com>
2024-02-04 11:02:32 +0100

    added content to SoapController

--- src/main/java/com/damsko/SoapVsRest/controller/SoapController.java
+++ src/main/java/com/damsko/SoapVsRest/controller/SoapController.java
@@ -1,4 +1,22 @@
 package com.damsko.SoapVsRest.controller;
 
+import com.damsko.SoapVsRest.model.Greeting;
+import com.damsko.SoapVsRest.service.SoapService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
 public class SoapController {
+
+    private static final String NAMESPACE_URI = "http://www.damsko.com/soapvsrest";
+
+    @Autowired
+    private SoapService soapService;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getGreetingRequest")
+    @ResponsePayload
+    public Greeting getGreeting() {
+        String message = soapService.generateSoapGreeting("SOAP User");
+        return new Greeting(message);
+    }
 }
