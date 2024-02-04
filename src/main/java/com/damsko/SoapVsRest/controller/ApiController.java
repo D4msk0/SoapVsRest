ef31db1d909ca002dd8a0c1803b9a46f6a569a75
Merry Damsko <m.d4msk0@gmail.com>
2024-02-04 10:55:14 +0100

    added content to ApiController (REST)

--- src/main/java/com/damsko/SoapVsRest/controller/ApiController.java
+++ src/main/java/com/damsko/SoapVsRest/controller/ApiController.java
@@ -1,4 +1,21 @@
 package com.damsko.SoapVsRest.controller;
 
+import com.damsko.SoapVsRest.model.Greeting;
+import com.damsko.SoapVsRest.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApiController {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping("/api/greet/{name}")
+    public Greeting greet(@PathVariable String name){
+        String message = greetingService.generateGreeting(name);
+        return new Greeting(message);
+    }
 }
