ab11b9d4c9ac67cadc719f88e45499b3d5baadca
Merry Damsko <m.d4msk0@gmail.com>
2024-02-04 10:57:16 +0100

    simple soap service

--- src/main/java/com/damsko/SoapVsRest/service/SoapService.java
+++ src/main/java/com/damsko/SoapVsRest/service/SoapService.java
@@ -1,4 +1,11 @@
 package com.damsko.SoapVsRest.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SoapService {
+
+    public String generateSoapGreeting(String name){
+        return "Hello from SOAP, " + name + "!";
+    }
 }
